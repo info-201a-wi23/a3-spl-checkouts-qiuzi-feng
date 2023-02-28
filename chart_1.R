6076df76cb724798ed7dd2b85e43cddff035f72f
qiuzi-feng <qfeng5@uw.edu>
2023-02-27 21:25:39 -0800

    finished chart 1

--- chart_1.R
+++ chart_1.R
@@ -1,1 +1,34 @@
+# Load libraries
+library("tidyr")
+library("dplyr")
+library("stringr")
+library("ggplot2")
+library("readr")
+library("scales")
+library("RColorBrewer")
 
+# Prepare the sub-dataframe for building the chart
+spl_df <- read.csv("~/Desktop/2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+harry_potter <- spl_df %>%
+  filter(str_detect(Title, "Harry Potter"))
+
+harry_potter <- harry_potter %>% mutate(date = paste0(CheckoutYear, "-", CheckoutMonth,  "-01" ))
+harry_potter$date <- as.Date(harry_potter$date, format = "%Y-%m-%d")
+
+sum_checkout_monthly <- harry_potter %>%
+  group_by(date, UsageClass) %>% 
+  summarize(all_checkouts = sum(Checkouts))
+
+material_types_checkout <- sum_checkout_monthly %>%
+  select("UsageClass", "all_checkouts", "date")
+
+# building the chart
+ggplot(data = material_types_checkout) +
+  geom_point(mapping = aes(x = date, y = all_checkouts, color = UsageClass)) +
+  scale_y_continuous(breaks = seq(0, 1200, 100)) +
+  labs(title = "Different Types of Harry Potter Books Checkouts Overtime",
+       x = "Month of the Year",
+       y = "Number of Checkouts",
+       color = "Book Types"
+  )
